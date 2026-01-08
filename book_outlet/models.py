c93f2e9bd6f65fb9bcea826dd7695523e731db08
Tanish0023 <tanish23aggarwal@gmail.com>
2026-01-08 20:11:43 +0530

    Completed Section 8

--- book_outlet/models.py
+++ book_outlet/models.py
@@ -4,18 +4,29 @@ from django.urls import reverse
 from django.utils.text import slugify
 
 # Create your models here.
+class Author(models.Model):
+    first_name = models.CharField(max_length=20)
+    last_name = models.CharField(max_length=20)
+
+    def full_name(self):
+        return f"{self.first_name} {self.last_name}"
+    
+    def __str__(self):
+        return self.full_name()
+
 class Book(models.Model):
     title = models.CharField(max_length=50)
     rating = models.IntegerField(validators=[MinValueValidator(1), MaxValueValidator(5)])
-    author = models.CharField(max_length=20, null=True)
-    slug = models.SlugField(default="", null=False)
+    author = models.ForeignKey(Author, on_delete=models.CASCADE, null=True)
+    slug = models.SlugField(default="", null=False,
+                            blank=True, db_index=True)
     
     def get_absolute_url(self):
         return reverse("book_detail", args=[self.slug])
 
-    def save(self, *args, **kwargs):
-        self.slug = slugify(self.title)
-        super().save(*args, **kwargs)
+    # def save(self, *args, **kwargs):
+    #     self.slug = slugify(self.title)
+    #     super().save(*args, **kwargs)
     
     def __str__(self):
         return f"{self.title} ({self.rating}) - {self.author}"
